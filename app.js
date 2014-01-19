8e5c6c748bee25fc89b207db5f501acc660c7270
Nik Garkusha <nik@openlab.ca>
2014-01-18 16:32:49 -0800

    port 80

--- app.js
+++ app.js
@@ -10,33 +10,37 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
-server.listen(process.env.PORT);
-//server.listen(9999);
+//server.listen(process.env.PORT);
+server.listen(80);
 
 var wsSrc = new WebSocket(config.sourceSocket);
 var wsDst = new WebSocketServer({server: server});
 
 wsSrc.on('open', function() {
-    console.log('Connected to ' + config.sourceSocket);
+    console.log('Connected to: ' + config.sourceSocket);
 });
 
-var wtf = [];
+var msg = [];
 
 wsSrc.on('message', function(data, flags) {
   var message = new Buffer(data).toString('base64');
-  wtf.push(message);
-  console.log("Now with: " + wtf.length);
+  msg.push(message);
+  console.log("Messages: " + msg.length);
 });
 
 var allSocks = [];
 
 wsDst.on('connection', function(ws) {
-  console.log("Welcome, " + ws.id);
+  console.log("Connected: " + ws.id);
   allSocks.push(ws);
 });
 
+ws.on('close', function(ws) {
+    console.log('Disconnected:' + ws.id);
+});
+
 var loop = setInterval(function() {
-  var one = wtf.pop();
+  var one = msg.pop();
   if (typeof one == 'string') {
     for(var i = allSocks.length - 1; i >= 0; i--) {
       if(allSocks[i].readyState == 1) {
