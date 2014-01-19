6d51343b052c183a80719ce75b0912b217e29e7c
Nik Garkusha <nik@openlab.ca>
2014-01-18 19:15:49 -0800

    fixed retry logic for connecting

--- app.js
+++ app.js
@@ -11,45 +11,61 @@ app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
 //server.listen(process.env.PORT);
-server.listen(80);
-
-var wsSrc = new WebSocket(config.sourceSocket);
-var wsDst = new WebSocketServer({server: server});
-
-wsSrc.on('open', function() {
-    console.log('Connected to: ' + config.sourceSocket);
-});
+server.listen(9999);
 
 var msg = [];
-
-wsSrc.on('message', function(data, flags) {
-  var message = new Buffer(data).toString('base64');
-  msg.push(message);
-  console.log("Messages: " + msg.length);
-});
-
-wsSrc.on('close', function(ws) {
-    console.log('Disconnected from: ' + config.sourceSocket);
-});
-
 var allSocks = [];
 
-wsDst.on('connection', function(ws) {
-  console.log("Client Connected: " + ws.id);
-  allSocks.push(ws);
-});
+function wsStart(){
+    var wsSrc = new WebSocket(config.sourceSocket);
+    wsSrc.on('open', function() {
+        console.log('Connected to: ' + config.sourceSocket);
 
-wsDst.on('close', function(ws) {
-    console.log('Client Disconnected:' + ws.id);
-});
+        wsSrc.on('message', function(data, flags) {
+          var message = new Buffer(data).toString('base64');
+          msg.push(message);
+          // console.log("Messages: " + msg.length);
+        });
 
-var loop = setInterval(function() {
-  var one = msg.pop();
-  if (typeof one == 'string') {
-    for(var i = allSocks.length - 1; i >= 0; i--) {
-      if(allSocks[i].readyState == 1) {
-        allSocks[i].send(new Buffer(one, "base64"));
-      }
-    }
-  }
-}, 50);
+        wsSrc.on('close', function(ws) {
+            console.log('Disconnected from: ' + config.sourceSocket);
+
+            // try to reconnect
+            setTimeout(wsStart(), 5000);
+        });
+
+        var wsDst = new WebSocketServer({server: server});
+
+        wsDst.on('connection', function(ws) {
+          console.log("Client Connected IP: " + ws._socket.remoteAddress + ":" + ws._socket.remotePort);
+          allSocks.push(ws);
+        });
+
+        wsDst.on('close', function(ws) {
+            console.log('Client Disconnected IP' + ws._socket.remoteAddress + ':' + ws._socket.remotePort);
+        });
+
+        wsDst.on('error', function(error) { console.log(error); });
+
+        var loop = setInterval(function() {
+          var one = msg.pop();
+          if (typeof one == 'string') {
+            for(var i = allSocks.length - 1; i >= 0; i--) {
+              if(allSocks[i].readyState == 1) {
+                allSocks[i].send(new Buffer(one, "base64"));
+              }
+            }
+          }
+        }, 50);
+
+    });
+
+    wsSrc.on('error', function(error) { console.log(error); setTimeout(wsStart(), 5000); });
+}
+
+wsStart();
+
+process.on('uncaughtException', function(err) {
+    // try to reconnect
+    setTimeout(wsStart(), 5000);
+});
